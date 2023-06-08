ebe8e86a3b701b7c12193f573572af1f60a1e7ea
1k32k2 <79296275+1k32k2@users.noreply.github.com>
2023-06-08 13:39:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,43 @@ import os
 import discord
 from discord.ext import commands
 
+client = commands.Bot(command_prefix='$', intents = discord.Intents.all())
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+@client.command()
+async def foo(ctx, *, arg):
+  await ctx.send(arg)
+
+@client.command()
+async def bulul(ctx):
+  await ctx.message.channel.send('lul non lul ngon')
+
+@client.command()
+async def buscu(ctx):
+  await ctx.message.channel.send('GAY')
+
+@client.command()
+async def minhdan(ctx):
+  await ctx.message.channel.send('chào chú bé <@809060184722112552> đình')
+# import discord
+# from discord.ext import commands
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+# intents = discord.Intents.default()
+# intents.message_content = True
+# bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+# @bot.event
+# async def on_ready():
+#     print(f"Logged in as {bot.user}")
+
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send('pong')
+
+# @bot.command()
+# async def hello(ctx):
+#     await ctx.send("Choo choo! 🚅")
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
